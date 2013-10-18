49a674d4a06577a844a58fa0dbe3188860661b1d
Thomas Boerger <tboerger@tbpro.de>
2013-10-18 16:02:20 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "timezone"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures timezone"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "timezone", "Installs/Configures timezone"
+
+supports "suse"
